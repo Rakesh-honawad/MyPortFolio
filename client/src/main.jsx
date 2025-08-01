9a36904bf28961aeed5565bde72d0e533fc0ccac
Rakesh Honawad <rakeshhonawad46@gmail.com>
2025-08-01 23:32:57 +0530

    Added Splash animaiton  customized as per requirment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import './styles/global.css';
+import SplashCursor from './components/SplashCursor.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <SplashCursor />
   </StrictMode>,
 )
